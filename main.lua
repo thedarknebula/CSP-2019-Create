6434353eebbf348fb409b803599a6b1aef373814
Ben <ThatsNoMoon.Alderaan@gmail.com>
2019-03-20 09:30:28 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,24 @@
+-- Draw the game to the screen
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
+end
+
+-- Update the game state, e.g. move player if they are holding a key
+function love.update()
+
+end
+
+-- Draw the background of stars
+function draw_stars()
+
+end
+
+-- Draw the player's ship
+function draw_player()
+
+end
+
+-- Draw all of the enemy ships
+function draw_enemies()
+
 end
