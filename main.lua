ebdc5366a5878972bac58c7f45b301cfd9188959
Ben <ThatsNoMoon.Alderaan@gmail.com>
2019-03-20 09:46:32 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,24 +1,38 @@
 -- Draw the game to the screen
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
-end
-
--- Update the game state, e.g. move player if they are holding a key
-function love.update()
-
+    drawEnemies()
 end
 
 -- Draw the background of stars
-function draw_stars()
+function drawStars()
 
 end
 
 -- Draw the player's ship
-function draw_player()
+function drawPlayer()
 
 end
 
+enemies = {
+    { x = 100, y = 100 }
+}
 -- Draw all of the enemy ships
-function draw_enemies()
+function drawEnemies()
+    for _, enemy in ipairs(enemies) do
+        love.graphics.polygon("fill", enemy.x, enemy.y, enemy.x - 10, enemy.y - 20, enemy.x + 10, enemy.y - 20)
+    end
+end
+
+-- Update the game state, e.g. move player if they are holding a key
+function love.update()
+    updatePlayer()
+    updateEnemies()
+end
+
+function updatePlayer()
+
+end
+
+function updateEnemies()
 
 end
